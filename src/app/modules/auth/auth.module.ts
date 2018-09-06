a79ecf1c1827e4408fa1c57bbc778fa3ea170e57
Jonathan Perez <jonathanperezmota@gmail.com>
2018-09-05 21:30:32 -0400

    Changing Auth module files names.

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthComponent } from './pages/auth/auth.page';
+import { AuthComponent } from './pages/auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
